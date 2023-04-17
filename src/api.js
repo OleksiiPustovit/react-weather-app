5523e5d1053d47330b7df7cd73c87da5451d47ee
Oleksii Pustovit <91pustovit91@gmail.com>
2023-04-17 13:06:04 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '089ee88022msh9de4540626c9db2p11fc91jsnc694eff94852',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = '8e5d8e930eee84b16534657483455f66';
